8d91e0247565a7712500efd8aab29a0c2fe45589
okanfdlh <ignadeafiranti@gmail.com>
2024-11-26 21:01:42 +0700

    rekrut

---
+++ footerb4.php
@@ -0,0 +1,28 @@
+<style>
+	footer p{
+		padding-top: 30px;
+		padding-bottom: 40px;
+		margin-top: 30px;
+	}
+	footer{
+		bottom: 0px;
+		margin-bottom: -20px;
+
+	}
+
+	footer p a{
+		text-decoration: none;
+		color: white;
+		
+	}
+</style>
+
+
+<footer>
+	<div class="bg-primary">
+
+           <div class="col-sm-12 text-center">
+              <p><a href="#">Copyright © 2024 PT. WINNICODE GARUDA TEKNOLOGI</a> </p>
+            </div>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Recruitment</title>
+
+	<link rel="stylesheet" href="bootstrap4/css/bootstrap.min.css">
+	<link rel="stylesheet" href="bootstrap4/css/custom.css">
+	
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+</head>
+<body style="background-image: url(bootstrap4/img/bg.jpg";>
+
+
+<?php 
+	include "navbarb4.php";
+ ?>
+
+<div class="container">
+	<div class="jumbotron jumbotron-fluid">
+	  <div class="container">
+	    <h1 class="display-3">PT. WINNICODE GARUDA TEKNOLOGI</h1>
+	    <p class="lead">PT. Winnicode Garuda Teknologi adalah perusahaan yang bergerak di bidang Media Publikasi dan Layanan Digital, didirikan pada tahun 2020 di Bandung. Kami hadir untuk menyediakan solusi publikasi dan digital yang inovatif bagi berbagai sektor industri. Fokus utama kami adalah pada publikasi berita, artikel, dan jurnalistik, serta menyediakan layanan digital yang mendukung transformasi dan pertumbuhan bisnis di era modern.<br>Dalam bidang Media Publikasi, PT. Winnicode Garuda Teknologi mengkhususkan diri dalam pembuatan dan distribusi konten-konten berkualitas seperti berita terkini, artikel mendalam, dan tulisan jurnalistik yang dirancang untuk memberikan informasi yang akurat, terpercaya, dan relevan kepada masyarakat. Kami berkomitmen untuk menyajikan konten yang tidak hanya informatif, tetapi juga berperan dalam membentuk opini publik dan mendukung perkembangan pengetahuan.</p>
+	  </div>
+	</div>
+
+
+<!-- carousel -->
+	<style>
+		.carousel-item img{
+			width:  100%;
+			max-height: 400px;
+		}
+	</style>
+		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+
+		 <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img class="d-block w-100" src="images/2.png" alt="First slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="images/4.png" alt="First slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="images/2.png" alt="Second slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="images/3.png" alt="Third slide">
+		    </div>
+		  </div>
+		  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+<!-- carousel -->
+
+<span style="height:40px;" ></span>
+
+	<!-- card group -->
+	<div class="row">
+	  <div class="col-sm-4">
+	    <div class="card">
+	      <div class="card-body">
+	        <h4 class="card-title">Visi</h4>
+	        <p class="card-text">Menjadi pemimpin dalam industri media publikasi dan layanan digital di Indonesia, dengan fokus pada inovasi, kualitas, dan integritas dalam setiap proyek yang kami jalankan.</p>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-sm-4">
+	    <div class="card">
+	      <div class="card-body">
+	        <h4 class="card-title">Misi</h4>
+	        <p class="card-text">Menyediakan layanan media publikasi yang kredibel dan berdampak, termasuk berita, artikel, dan konten jurnalistik yang berkualitas.<br>Mengembangkan solusi digital yang efisien dan efektif untuk mendukung transformasi bisnis ke arah digitalisasi.<br>Membangun kemitraan yang berkelanjutan dengan klien melalui layanan yang unggul dan kepuasan pelanggan.<br>Menginspirasi dan mendorong perkembangan teknologi di Indonesia melalui inovasi dan keahlian kami.</p>
+	      </div>
+	    </div>
+	  </div>
+
+	  <div class="col-sm-4">
+	    <div class="card">
+	      <div class="card-body">
+	        <h4 class="card-title">Tentang</h4>
+	        <p class="card-text">Nama perusahaan : PT. WINNICODE GARUDA TEKNOLOGI</p>
+	       	<p class="card-text">E-mail : admin@winnicode.com</p>
+			<p class="card-text">Alamat	: Bantul,Yogyakarta</p>
+			<p class="card-text">CalL Center: 6285159932501 (24 Jam)</p>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<!-- card group -->
+
+
+
+</div>
+
+
+<?php 
+	include "footerb4.php";
+ ?>
+
+
+	<script src="bootstrap4/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</body>
+</html>
